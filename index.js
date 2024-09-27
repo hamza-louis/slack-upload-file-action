b2598d42d01c2cbe6bc4dd49244a9ae8d7cb8374
Hamza Louis <h.louis@bankaletihad.com>
2024-09-27 22:18:03 +0300

    refactor inputs names

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const web = new WebClient(token);
 
 async function uploadFile() {
     try {
-      const path = core.getInput('path');
-      const channel_id = core.getInput('slack_channel_id');
-      const initial_comment = core.getInput('initial_comment');
-      const filename = core.getInput('filename');
+      const slack_file_path = core.getInput('slack_file_path');
+      const slack_channel_id = core.getInput('slack_channel_id');
+      const slack_file_upload_initial_comment = core.getInput('slack_file_upload_initial_comment');
+      const slack_file_upload_filename = core.getInput('slack_file_upload_filename');
       const result = await web.filesUploadV2({
-        channel_id: channel_id,
-        initial_comment: initial_comment,
-        file: path,
-        filename: filename,
+        channel_id: slack_channel_id,
+        initial_comment: slack_file_upload_initial_comment,
+        file: slack_file_path,
+        filename: slack_file_upload_filename,
       });
       core.setOutput("result", result);
-      console.log(`The File ${filename} has been uploaded successfully:`, result);
+      console.log(`The File ${slack_file_upload_filename} has been uploaded successfully:`, result);
     } catch (error) {
       console.error('Error uploading file:', error);
     }
